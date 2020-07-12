27fe268d2d8b336db5d4f1b90ba984de5845a7aa
xuegang.liu <xuegang.liu@moji.com>
2020-07-12 23:59:25 +0800

    week03 homework

---
+++ Week_03/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -0,0 +1,81 @@
+//根据一棵树的前序遍历与中序遍历构造二叉树。
+//
+// 注意:
+//你可以假设树中没有重复的元素。
+//
+// 例如，给出
+//
+// 前序遍历 preorder = [3,9,20,15,7]
+//中序遍历 inorder = [9,3,15,20,7]
+//
+// 返回如下的二叉树：
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// Related Topics 树 深度优先搜索 数组
+
+package leetcode.editor.cn;
+
+import java.util.Stack;
+
+//Java：从前序与中序遍历序列构造二叉树
+public class P105ConstructBinaryTreeFromPreorderAndInorderTraversal{
+    public static void main(String[] args) {
+        Solution solution = new P105ConstructBinaryTreeFromPreorderAndInorderTraversal().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+
+ class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+  }
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder.length == 0) {
+            return null;
+        }
+        Stack<TreeNode> roots = new Stack<TreeNode>();
+        int pre = 0;
+        int in = 0;
+        //先序遍历第一个值作为根节点
+        TreeNode curRoot = new TreeNode(preorder[pre]);
+        TreeNode root = curRoot;
+        roots.push(curRoot);
+        pre++;
+        //遍历前序遍历的数组
+        while (pre < preorder.length) {
+            //出现了当前节点的值和中序遍历数组的值相等，寻找是谁的右子树
+            if (curRoot.val == inorder[in]) {
+                //每次进行出栈，实现倒着遍历
+                while (!roots.isEmpty() && roots.peek().val == inorder[in]) {
+                    curRoot = roots.peek();
+                    roots.pop();
+                    in++;
+                }
+                //设为当前的右孩子
+                curRoot.right = new TreeNode(preorder[pre]);
+                //更新 curRoot
+                curRoot = curRoot.right;
+                roots.push(curRoot);
+                pre++;
+            } else {
+                //否则的话就一直作为左子树
+                curRoot.left = new TreeNode(preorder[pre]);
+                curRoot = curRoot.left;
+                roots.push(curRoot);
+                pre++;
+            }
+        }
+        return root;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
